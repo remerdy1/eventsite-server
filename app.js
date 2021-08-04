5b66a8d219f76451b5ab21a2b6e72d93ff81da39
remerdy1 <62181722+remerdy1@users.noreply.github.com>
2021-08-04 04:36:59 +0100

    Add gitignore and handle errors

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const express = require("express");
 const axios = require('axios');
+const cors = require("cors");
+
 require('dotenv').config();
 
 const app = express();
-
-const port = process.env.PORT || 8050
+app.use(cors());
+const port = process.env.PORT || 8050;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 app.post("/events", async (req, res) => {
     const apiKey = process.env.API_KEY;
 
-    city = req.body.city
-    countryCode = req.body.country
-    const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=${city}&countryCode=${countryCode}`
+    city = req.body.city;
+    countryCode = req.body.country;
+    // TODO: Filter results, remove duplicates, remove cancelled events etc
+    const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=${city}&countryCode=${countryCode}`;
 
     // Fetch events using the ticketmaster api
     try {
         const response = await axios.get(url);
-        const events = response.data._embedded.events
+        const events = response.data._embedded.events;
 
-        res.send(events)
-    }
-    // TODO: Handle errors
-    catch (e) {
-        console.log(e)
+        res.send(events);
+    } catch (e) {
+        res.status(404).send("Unable to find events")
     }
 
 
 })
 
-app.listen(port, () => console.log(`App listening on port ${port}`))
+app.listen(port, () => console.log(`App listening on port ${port}`));
